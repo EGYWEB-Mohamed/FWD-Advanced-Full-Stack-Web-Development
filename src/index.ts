10fb1c9e1ba3864c7ea0e8056e1c69de997c6026
Mohamed Said <ceo@egyweb.info>
2022-03-06 19:51:15 +0200

    ..

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-const a = 5;
-const b = ('2 Medo' as unknown) as number;
-const c = ("Medo" as unknown) as number;
-
-let mult = (num1:number , num2:number) => {
-  return num1 * num2;
-}
-
-console.log(mult(a,c))
